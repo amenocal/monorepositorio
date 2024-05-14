4fa2962b6ebd2e9f1870a976d73c8cbf001d76bc
Alejandro Menocal <amenocal@github.com>
2024-05-14 16:57:06 -0500

    mensaje

--- cmd/root.go
+++ cmd/root.go
@@ -30,13 +30,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "monorepositorio",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Un ejemplo de un cli con cobra",
+	Long: `Esta es una applicacion de ejemplo para mostrar como se puede crear un CLI
+	con GoLang y Cobra.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -61,4 +57,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	rootCmd.Flags().StringP("mensaje", "m", "Hola Mundo!", "Mensaje a imprimir")
 }
